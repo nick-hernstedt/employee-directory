7767c61814e9bfe690b12e480f065d5f68c5b7b4
copiousamounts <nick.hernstedt@gmail.com>
2020-05-14 16:58:01 -0700

    got it working

--- src/components/table/table-rows/index.js
+++ src/components/table/table-rows/index.js
@@ -1,85 +1,59 @@
 import React, { Component } from "react";
-import Axios from "axios";
-import Loader from "../loading";
-import ErrorAlert from "../error";
-import TableRows from "./table-rows";
+import "./style.css";
 
-class Table extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      count: 0,
-      isLoading: true,
-      error: "",
-      search: "",
-      directionOfSort: "descending",
-    };
-    this.sortBy = this.sortBy.bind(this);
-  }
-
-  componentDidMount() {
-    Axios.get("https://randomuser.me/api/?seed=employee&results=30&nat=us")
-      .then((res) => {
-        this.setState({
-          data: res.data.results,
-          count: res.data.results.length,
-          isLoading: false,
-          error: "",
-        });
-      })
-      .catch((err) => {
-        this.setState({
-          isLoading: false,
-          error: err.message,
-        });
-      });
-  }
-
-  sortBy(title) {
-    const newArray = [...this.state.data];
-
-    if (this.state.directionOfSort === "descending") {
-      let sortedData = newArray.sort((a, b) => {
-        if (a.name[title] < b.name[title]) return -1;
-        if (a.name[title] > b.name[title]) return 1;
-        return 0;
-      });
-      this.setState({
-        data: sortedData,
-        directionOfSort: "ascending",
-      });
-    } else {
-      let sortedData = newArray.sort((a, b) => {
-        if (a.name[title] > b.name[title]) return -1;
-        if (a.name[title] < b.name[title]) return 1;
-        return 0;
-      });
-      this.setState({
-        data: sortedData,
-        directionOfSort: "descending",
-      });
-    }
-  }
-
-  handleInputChange(event) {
-    let query = event.target.value;
-    this.setState({ search: query });
-  }
-
+class TableRows extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="col">
-          {this.state.isLoading === true && <Loader />}
-          {this.state.error !== "" && <ErrorAlert error={this.state.error} />}
-          {this.state.isLoading === false && this.state.count !== 0 && (
-            <TableRows emplyData={this.state} sort={this} />
-          )}
-        </div>
+      <div className="table-responsive">
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col"></th>
+              <th scope="col" className="text-secondary">
+                First{" "}
+                <i
+                  className="fas fa-sort cursor"
+                  onClick={() => this.props.sort.sortBy("first")}
+                ></i>
+              </th>
+              <th scope="col" className="text-secondary">
+                Last{" "}
+                <i
+                  className="fas fa-sort cursor"
+                  onClick={() => this.props.sort.sortBy("last")}
+                ></i>
+              </th>
+              <th scope="col" className="text-secondary">
+                Email
+              </th>
+              <th scope="col" className="text-secondary">
+                Cell
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.emplyData.data.map((emp, i) => {
+              return (
+                <tr className="cursor" key={i}>
+                  <th scope="row">
+                    <img
+                      id="thumbnail"
+                      src={emp.picture.thumbnail}
+                      alt="employee pic"
+                    />
+                  </th>
+                  <td className="align-middle">{emp.name.first}</td>
+                  <td className="align-middle">{emp.name.last}</td>
+                  <td className="align-middle">{emp.email}</td>
+                  <td className="align-middle">{emp.cell}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
 }
 
-export default Table;
+export default TableRows;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import App from "./components/app";
 
-ReactDOM.render(
-  <App />,
-
-  document.getElementById("root")
-);
-
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
