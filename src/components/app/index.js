0aa0b642476aad0abe5aaac9319bb2ce171fd164
copiousamounts <nick.hernstedt@gmail.com>
2020-05-14 16:51:59 -0700

    added app to componenet folder

---
+++ src/components/app/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Table from "../../components/table";
+import Navbar from "../../components/navbar";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <Navbar />
+        </header>
+
+        <main className="container">
+          <Table />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
