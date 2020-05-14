e6eb6df3505e48aa86d133d029363fb18b5cc524
copiousamounts <nick.hernstedt@gmail.com>
2020-05-14 15:12:55 -0700

    created loader component

---
+++ src/components/loading/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div class="d-flex justify-content-center">
+      <div
+        className="spinner-border text-primary"
+        style={{ width: "3rem", height: "3rem" }}
+        role="status"
+      >
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
